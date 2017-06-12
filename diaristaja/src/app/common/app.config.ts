17ac31771a21355d373cb995054544dd5a84b21c
Aline Santos <aline.santos@concrete.com.br>
2017-06-12 09:03:08 -0300

    altera endpoint para heroku

--- diaristaja/src/app/common/app.config.ts
+++ diaristaja/src/app/common/app.config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-    // public static readonly APP_URL = "https://diaristaja.herokuapp.com/diarista"
-    public static readonly APP_URL = "http://localhost:8080/diarista"
+    public static readonly APP_URL = "https://diaristaja.herokuapp.com/diarista"
+    // public static readonly APP_URL = "http://localhost:8080/diarista"
 }
